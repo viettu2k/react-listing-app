b8749f6c0207ad51ae902c83c809d28e88591cfa
viettu2k <doanviettu2k@gmail.com>
2022-02-12 21:04:25 +0700

    styling TopBar component

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -3,5 +3,11 @@ import "./TopBar.css";
 
 // React component returns JSX kind or markup
 export default function TopBar() {
-  return <div className="topbar"> Top Bar</div>;
+  return (
+    <div className="topbar">
+      <div className="topbar-left">Left</div>
+      <div className="topbar-center">Center</div>
+      <div className="topbar-right">Right</div>
+    </div>
+  );
 }
