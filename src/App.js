75dff4f62c6742fff0340b1ddf51a115287da8a7
viettu2k <doanviettu2k@gmail.com>
2022-02-12 16:25:31 +0700

    creating TopBar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import TopBar from "./components/topbar/TopBar";
+
 function App() {
-  return <div className="App">Listing App</div>;
+  return (
+    <div className="App">
+      Listing App
+      <TopBar />
+    </div>
+  );
 }
 
 export default App;
