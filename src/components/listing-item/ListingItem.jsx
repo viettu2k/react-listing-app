276ca2141fcd39d1612b40d9e8f8db1cf9f4f2d5
viettu2k <doanviettu2k@gmail.com>
2022-02-13 20:14:10 +0700

    working on Listing overview screen

--- src/components/listing-item/ListingItem.jsx
+++ src/components/listing-item/ListingItem.jsx
@@ -5,11 +5,18 @@ export default function ListingItem() {
   return (
     <div className="listing-item">
       <img
+        className="listing-img"
         src="https://images.pexels.com/photos/2606383/pexels-photo-2606383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
       />
       <div className="listing-details">
-        <div className="listing-categories">categories</div>
+        <div className="listing-categories">
+          <span className="listing-category">Buy</span>
+          <span className="listing-category">Sell</span>
+        </div>
+        <span className="listing-title">Awesome property available.</span>
+        <hr />
+        <span className="listing-time">2 days ago</span>
       </div>
     </div>
   );
--- src/components/listing/Listing.jsx
+++ src/components/listing/Listing.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-// import ListingItem from "../listing-item/ListingItem";
+import ListingItem from "../listing-item/ListingItem";
 import "./Listing.css";
 
 export default function Listing() {
-  return <div className="listing-section">All Properties</div>;
+  return (
+    <div className="listing-section">
+      All Properties
+      <ListingItem />
+      <ListingItem />
+      <ListingItem />
+    </div>
+  );
 }
