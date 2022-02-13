965a40c6ec01372c39c5030a1b72167ec4d3a3f2
viettu2k <doanviettu2k@gmail.com>
2022-02-13 19:55:28 +0700

    using css variables

--- src/components/listing/Listing.jsx
+++ src/components/listing/Listing.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import ListingItem from "../listing-item/ListingItem";
+// import ListingItem from "../listing-item/ListingItem";
 import "./Listing.css";
 
 export default function Listing() {
-  return (
-    <div className="listing-section">
-      All Properties
-      <ListingItem />
-      <ListingItem />
-      <ListingItem />
-      <ListingItem />
-      <ListingItem />
-      <ListingItem />
-    </div>
-  );
+  return <div className="listing-section">All Properties</div>;
 }
