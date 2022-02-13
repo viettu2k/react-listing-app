4a4e3e98614da8d259b4fb08842aa09627a2b88b
viettu2k <doanviettu2k@gmail.com>
2022-02-13 21:03:31 +0700

    styling the ListingDetail Page

--- src/components/listing-detail/ListingDetail.jsx
+++ src/components/listing-detail/ListingDetail.jsx
@@ -2,5 +2,19 @@ import React from "react";
 import "./ListingDetail.css";
 
 export default function ListingDetail() {
-  return <div className="listing-detail">ListingDetail</div>;
+  return (
+    <div className="listing-detail">
+      <div className="listing-detail-content">
+        <img
+          src="https://images.pexels.com/photos/2606383/pexels-photo-2606383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt=""
+        />
+        <h2 className="listing-detail-title">Awesome property</h2>
+        <div className="listing-detail-action">
+          <i class="fas fa-edit"></i>
+          <i class="fas fa-trash-alt"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
