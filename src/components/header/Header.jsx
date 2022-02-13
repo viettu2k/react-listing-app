81bdb2fd8d8bfd23c220a9bf5a258e2115007b34
viettu2k <doanviettu2k@gmail.com>
2022-02-13 09:35:23 +0700

    styling hero section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import "./Header.css";
 
 export default function Header() {
-  return <div>Header Section</div>;
+  return (
+    <div className="header-section">
+      <div className="hero-text">
+        <span className="hero-text-title">Listings</span>
+        <span className="hero-text-subtitle">Listing Application</span>
+      </div>
+      <img
+        src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+    </div>
+  );
 }
