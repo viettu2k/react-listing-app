0fee8ee9dfb4a1519e356e5e7310455c74d4c70f
viettu2k <doanviettu2k@gmail.com>
2022-02-13 21:12:03 +0700

    working on Listing Detail page meta data section

--- src/components/listing-detail/ListingDetail.jsx
+++ src/components/listing-detail/ListingDetail.jsx
@@ -9,11 +9,42 @@ export default function ListingDetail() {
           src="https://images.pexels.com/photos/2606383/pexels-photo-2606383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
           alt=""
         />
-        <h2 className="listing-detail-title">Awesome property</h2>
-        <div className="listing-detail-action">
-          <i class="fas fa-edit"></i>
-          <i class="fas fa-trash-alt"></i>
+        <h2 className="listing-detail-title">
+          Awesome property
+          <div className="listing-detail-action">
+            <i class="listing-detail-action-icon fas fa-edit"></i>
+            <i class="listing-detail-action-icon fas fa-trash-alt"></i>
+          </div>
+        </h2>
+        <div className="listing-detail-meta-info">
+          <span className="listing-detail-author">
+            Author - <strong>Timber</strong>
+          </span>
+          <span className="listing-detail-time">1 day ago</span>
         </div>
+
+        <p className="listing-detail-description">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore,
+          veritatis voluptatum? Quo, culpa corporis vero dolores sint ad,
+          reiciendis ea suscipit nesciunt accusamus similique autem maxime ut
+          eveniet! Eius, animi. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Labore, veritatis voluptatum? Quo, culpa corporis
+          vero dolores sint ad, reiciendis ea suscipit nesciunt accusamus
+          similique autem maxime ut eveniet! Eius, animi. Lorem ipsum, dolor sit
+          amet consectetur adipisicing elit. Labore, veritatis voluptatum? Quo,
+          culpa corporis vero dolores sint ad, reiciendis ea suscipit nesciunt
+          accusamus similique autem maxime ut eveniet! Eius, animi. Lorem ipsum,
+          dolor sit amet consectetur adipisicing elit. Labore, veritatis
+          voluptatum? Quo, culpa corporis vero dolores sint ad, reiciendis ea
+          suscipit nesciunt accusamus similique autem maxime ut eveniet! Eius,
+          animi. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Labore, veritatis voluptatum? Quo, culpa corporis vero dolores sint
+          ad, reiciendis ea suscipit nesciunt accusamus similique autem maxime
+          ut eveniet! Eius, animi. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Labore, veritatis voluptatum? Quo, culpa corporis
+          vero dolores sint ad, reiciendis ea suscipit nesciunt accusamus
+          similique autem maxime ut eveniet! Eius, animi.
+        </p>
       </div>
     </div>
   );
