9c1b8c71d5c1b6a31f7690dfc718f63b2695a093
viettu2k <doanviettu2k@gmail.com>
2022-02-13 15:23:57 +0700

    styling sidebar section

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./Sidebar.css";
 
 export default function Sidebar() {
-  return <div className="sidebar-section">SideBar</div>;
+  return (
+    <div className="sidebar-section">
+      <div className="sidebar-subsection">
+        <span className="sidebar-title">about me</span>
+        <img
+          className="sidebar-"
+          src="https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem iusto
+          similique illo laudantium, nam reiciendis? Inventore, eius similique
+          doloremque vel, quisquam minima iure architecto iusto voluptatibus
+          neque, illum odio deleniti.
+        </p>
+      </div>
+      <div className="sidebar-subsection">
+        <span className="sidebar-title">categories</span>
+        <ul className="sidebar-list">
+          <li className="sidebar-category">Buy</li>
+          <li className="sidebar-category">Sell</li>
+          <li className="sidebar-category">Rent</li>
+          <li className="sidebar-category">Hostel</li>
+          <li className="sidebar-category">B & B</li>
+          <li className="sidebar-category">Hotel</li>
+        </ul>
+      </div>
+      <div className="sidebar-subsection">
+        <span className="sidebar-title">follow us on:</span>
+        <div className="sidebar-social-icons">
+          <i className="sidebar-social-icon fab fa-instagram-square"></i>
+          <i className="sidebar-social-icon fab fa-facebook-square"></i>
+          <i className="sidebar-social-icon fab fa-linkedin"></i>
+          <i className="sidebar-social-icon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
