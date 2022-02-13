2c3dad238d725c0051887addc8e777ee187dd745
viettu2k <doanviettu2k@gmail.com>
2022-02-13 09:13:08 +0700

    adding Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import TopBar from "./components/topbar/TopBar";
 import React from "react";
+import TopBar from "./components/topbar/TopBar";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      Listing App
       <TopBar />
+      <Header />
     </div>
   );
 }
