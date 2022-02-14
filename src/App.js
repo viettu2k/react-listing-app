395847b2b5d4f462e5fabbc21e74da9c5088fabf
viettu2k <doanviettu2k@gmail.com>
2022-02-14 10:42:57 +0700

    installing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import TopBar from "./components/topbar/TopBar";
 import CreateListing from "./pages/create-listing/CreateListing";
-// import Home from "./pages/homepage/Home";
-// import Detail from "./pages/listingdetail/Detail";
+import Home from "./pages/homepage/Home";
+import Detail from "./pages/listingdetail/Detail";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      {/* <Home /> */}
-      {/* <Detail /> */}
+      <Home />
+      <Detail />
       <CreateListing />
     </div>
   );
